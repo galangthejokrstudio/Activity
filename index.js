9ba8b9349ffe02552cec4f6af3e7f7cd5fa61f8f
Galang TehjokR <apikeyzlang@gmail.com>
2024-12-17 15:18:14 +0700

    Update index.js

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ client.on('ready', async () => {
         .setState('diewe atau ngewe')
         .setName('Genshin Impact')
         .setDetails('Lv 58 | langtzy')
-        .setAssetsLargeImage('https://media.discordapp.net/attachments/1146087440357011666/1242687267320102953/IMG-20240424-WA0603.jpg')
+        .setAssetsLargeImage('https://media.discordapp.net/attachments/1263708674505834562/1316266459218972702/ab0064976a88d3f88cfc4217d6829d13.gif?ex=6762552a&is=676103aa&hm=bfbf90068fd95a214f2c872d627c10f3af84eec24310814ffae85f0975ed3378&')
         .setAssetsLargeText('Visit this')
-        .setAssetsSmallImage('https://media.discordapp.net/attachments/1146087440357011666/1242688541780217930/Diamond_JE3_BE3.png')
+        .setAssetsSmallImage('https://media.discordapp.net/attachments/1263708674505834562/1317873295156314213/ddd67a00297de983b9c275294f4e66b6.jpg?ex=67623ee6&is=6760ed66&hm=8c803409198ac7fb5e133c81b3ddd1765d94b8d923bfe026e984b34315bcf91f&')
         .setAssetsSmallText('Verified')
         .addButton('Yt Channel', 'https://www.youtube.com/channel/@galangthejokr')
         .addButton('Discord Server', 'https://discord.com/invite/YsynWR8Uc4');
@@ -29,7 +29,7 @@ client.on('ready', async () => {
     client.user.setPresence({ status: 'dnd' });
 });
 
-client.login(process.env.TOKEN);
+client.login("OTc3NTAzMDkxMzkxODYwNzY5.GITKxa.8Mf6Y5k0M_rcCjriY4Na3hKisr53y53gbQkV3c");
 
 
 
